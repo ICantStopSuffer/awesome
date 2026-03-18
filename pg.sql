24080129e6b1374fa2317761800b9912d6fb14cd
ICantStopSuffer <sasha.plevovskih@yandex.ru>
2026-03-18 10:33:57 +0500

    sql

---
+++ pg.sql
@@ -0,0 +1,33 @@
+create table Departments (
+	id serial primary key,
+	name text
+);
+
+create table Projects (
+	id serial primary key,
+	name text
+);
+
+create table Employees (
+	id serial primary key
+	name text,
+	department int references Departments(id)
+);
+
+create table Skills (
+	id serial primary key
+	name text,
+	description text,
+)
+
+create table SkillEmployeeBridge (
+	id serial primary key,
+	employee int references Employees(id),
+	skill int references Skills(id)
+);
+
+create table ProjectEmployeeBridge (
+	id serial primary key,
+	employee int references Employees(id),
+	project int references Projects(id)
+);
